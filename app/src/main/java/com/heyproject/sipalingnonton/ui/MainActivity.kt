46a2af4d27535cef080ac909b6314c7ce7ef747a
yayanrw <yayanraw@gmail.com>
2022-07-25 21:20:06 +0700

    remove elevation action bar

--- app/src/main/java/com/heyproject/sipalingnonton/ui/MainActivity.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/ui/MainActivity.kt
@@ -10,5 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setActionBar()
+    }
+
+    private fun setActionBar() {
+        supportActionBar?.apply {
+            elevation = 0.0F
+        }
     }
 }
