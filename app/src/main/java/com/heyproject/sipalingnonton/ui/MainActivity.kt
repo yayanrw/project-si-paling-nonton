b7f940648217fa42dea6210d19d6d46c6af96d71
yayanrw <yayanraw@gmail.com>
2022-07-25 20:56:24 +0700

    binding on MainActivity.kt

--- app/src/main/java/com/heyproject/sipalingnonton/ui/MainActivity.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/ui/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.heyproject.sipalingnonton.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.heyproject.sipalingnonton.R
+import androidx.appcompat.app.AppCompatActivity
+import com.heyproject.sipalingnonton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
