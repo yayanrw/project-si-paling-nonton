27b36ff002757ee3516bc5d41af062b9166d3f94
yayanrw <yayanraw@gmail.com>
2022-08-21 11:12:27 +0700

    add movienowplaying dto

---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/dto/DatesDto.kt
@@ -0,0 +1,11 @@
+package com.heyproject.sipalingnonton.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class DatesDto(
+    @SerializedName("maximum")
+    val maximum: String,
+    @SerializedName("minimum")
+    val minimum: String
+)
---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/response/MovieNowPlayingResponse.kt
@@ -0,0 +1,19 @@
+package com.heyproject.sipalingnonton.data.remote.response
+
+
+import com.google.gson.annotations.SerializedName
+import com.heyproject.sipalingnonton.data.remote.dto.DatesDto
+import com.heyproject.sipalingnonton.data.remote.dto.MovieDto
+
+data class MovieNowPlayingResponse(
+    @SerializedName("dates")
+    val dates: DatesDto,
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+    val results: List<MovieDto>,
+    @SerializedName("total_pages")
+    val totalPages: Int,
+    @SerializedName("total_results")
+    val totalResults: Int
+)
