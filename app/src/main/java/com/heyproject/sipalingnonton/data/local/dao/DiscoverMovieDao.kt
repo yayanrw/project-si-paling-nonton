f3d1f592262ba6c4884a58212a00686105e072cf
yayanrw <yayanraw@gmail.com>
2022-08-21 22:39:52 +0700

    add return types

--- app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/DiscoverMovieDao.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/DiscoverMovieDao.kt
@@ -6,10 +6,10 @@ import com.heyproject.sipalingnonton.data.local.entity.DiscoverMovieEntity
 @Dao
 interface DiscoverMovieDao {
     @Query("SELECT * FROM discover_movies")
-    suspend fun getMovies()
+    suspend fun getMovies(): List<DiscoverMovieEntity>
 
     @Query("SELECT * FROM discover_movies WHERE is_favorite = 1")
-    suspend fun getFavoriteMovies()
+    suspend fun getFavoriteMovies(): List<DiscoverMovieEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovie(discover_movies: List<DiscoverMovieEntity>)
--- app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/MovieDetailDao.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/MovieDetailDao.kt
@@ -1,13 +1,15 @@
 package com.heyproject.sipalingnonton.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.heyproject.sipalingnonton.data.local.entity.MovieDetailEntity
 
+@Dao
 interface MovieDetailDao {
     @Query("SELECT A.*, B.is_favorite FROM movies_detail A INNER JOIN discover_movies B ON A.id = B.id WHERE A.id = :movieId")
-    suspend fun getMovieDetail(movieId: Int)
+    suspend fun getMovieDetail(movieId: Int): MovieDetailEntity
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovieDetail(movieDetailEntity: MovieDetailEntity)
