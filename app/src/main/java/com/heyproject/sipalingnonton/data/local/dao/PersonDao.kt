efdd122622768b8d99f45b0df664d67a0630b768
yayanrw <yayanraw@gmail.com>
2022-08-21 23:56:13 +0700

    person local

---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/PersonDao.kt
@@ -0,0 +1,18 @@
+package com.heyproject.sipalingnonton.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.heyproject.sipalingnonton.data.local.entity.PersonEntity
+
+@Dao
+interface PersonDao {
+    @Query("SELECT * FROM persons WHERE id = :id")
+    suspend fun getPerson(id: Int): PersonEntity
+
+    @Insert
+    suspend fun insertPerson(person: PersonEntity)
+
+    @Query("DELETE FROM persons WHERE created_at <= (strftime('%s','now', '-30 day') * 1000)")
+    suspend fun deletePersons()
+}
---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/entity/PersonEntity.kt
@@ -0,0 +1,42 @@
+package com.heyproject.sipalingnonton.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "persons")
+data class PersonEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "also_known_as")
+    val alsoKnownAs: List<String>,
+
+    @ColumnInfo(name = "biography")
+    val biography: String,
+
+    @ColumnInfo(name = "birthday")
+    val birthday: String,
+
+    @ColumnInfo(name = "deathday")
+    val deathday: String,
+
+    @ColumnInfo(name = "place_of_birth")
+    val placeOfBirth: String,
+
+    @ColumnInfo(name = "profile_path")
+    val profilePath: String,
+
+    @ColumnInfo(name = "imdb_id")
+    val imdbId: String,
+
+    @ColumnInfo(name = "known_for_department")
+    val knownForDepartment: String,
+
+    @ColumnInfo(name = "created_at")
+    val createdAt: Long = System.currentTimeMillis()
+)
