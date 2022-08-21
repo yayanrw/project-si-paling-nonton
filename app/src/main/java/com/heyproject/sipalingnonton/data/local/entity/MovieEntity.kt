d0d4dd2d1911e15231a6d74ca8d2b1c23da0a001
yayanrw <yayanraw@gmail.com>
2022-08-21 19:28:13 +0700

    add MovieEntity.kt

---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/entity/MovieEntity.kt
@@ -0,0 +1,36 @@
+package com.heyproject.sipalingnonton.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class MovieEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "overview")
+    val overview: String,
+
+    @ColumnInfo(name = "backdrop_path")
+    val backdropPath: String,
+
+    @ColumnInfo(name = "poster_path")
+    val posterPath: String,
+
+    @ColumnInfo(name = "release_date")
+    val releaseDate: String,
+
+    @ColumnInfo(name = "vote_average")
+    val voteAverage: Double,
+
+    @ColumnInfo(name = "genre_ids")
+    val genreIds: List<Int>,
+
+    @ColumnInfo(name = "is_favorite")
+    val isFavorite: Boolean = false
+)
