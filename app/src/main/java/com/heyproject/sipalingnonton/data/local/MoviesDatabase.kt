4519b187172aa54ba5f653c855c611bb65bb3d66
yayanrw <yayanraw@gmail.com>
2022-08-22 00:20:31 +0700

    add MoviesDatabase.kt

---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/MoviesDatabase.kt
@@ -0,0 +1,26 @@
+package com.heyproject.sipalingnonton.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.heyproject.sipalingnonton.data.local.dao.*
+import com.heyproject.sipalingnonton.data.local.entity.*
+
+@Database(
+    entities = [
+        GenreEntity::class,
+        MovieCreditEntity::class,
+        MovieDetailEntity::class,
+        MovieEntity::class,
+        PersonEntity::class,
+        SimilarMovieEntity::class,
+    ],
+    version = 1
+)
+abstract class MoviesDatabase : RoomDatabase() {
+    abstract val genreDao: GenreDao
+    abstract val movieCreditDao: MovieCreditDao
+    abstract val movieDao: MovieDao
+    abstract val movieDetailDao: MovieDetailDao
+    abstract val personDao: PersonDao
+    abstract val similarMovieDao: SimilarMovieDao
+}
