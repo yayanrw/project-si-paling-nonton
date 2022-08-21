a194e0b62e2ef3f2f382b45abce54c8447797d69
yayanrw <yayanraw@gmail.com>
2022-08-21 23:59:33 +0700

    on conflict strategy

--- app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/PersonDao.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/PersonDao.kt
@@ -2,6 +2,7 @@ package com.heyproject.sipalingnonton.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.heyproject.sipalingnonton.data.local.entity.PersonEntity
 
@@ -10,7 +11,7 @@ interface PersonDao {
     @Query("SELECT * FROM persons WHERE id = :id")
     suspend fun getPerson(id: Int): PersonEntity
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPerson(person: PersonEntity)
 
     @Query("DELETE FROM persons WHERE created_at <= (strftime('%s','now', '-30 day') * 1000)")
