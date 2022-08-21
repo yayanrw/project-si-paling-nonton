320d46e6256c8c72dad7b5c59c2e773d566eda9b
yayanrw <yayanraw@gmail.com>
2022-08-21 23:30:02 +0700

    rename movie

--- app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/MovieDao.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/MovieDao.kt
@@ -11,9 +11,24 @@ interface MovieDao {
     @Query("SELECT * FROM movies WHERE is_favorite = 1")
     suspend fun getFavoriteMovies(): List<MovieEntity>
 
+    @Query("SELECT * FROM movies WHERE is_trending = 1")
+    suspend fun getTrendingMovies(): List<MovieEntity>
+
+    @Query("SELECT * FROM movies WHERE is_now_playing = 1")
+    suspend fun getNowPlayingMovies(): List<MovieEntity>
+
+    @Query("SELECT * FROM movies WHERE is_top_rated = 1")
+    suspend fun getTopRatedMovies(): List<MovieEntity>
+
+    @Query("SELECT * FROM movies WHERE title LIKE '%' || :search || '%'")
+    suspend fun searchMovies(search: String): List<MovieEntity>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovie(discover_movies: List<MovieEntity>)
+    suspend fun insertMovie(movies: List<MovieEntity>)
 
     @Update
-    suspend fun updateFavoriteMovie(discover_movie: MovieEntity)
+    suspend fun updateFavoriteMovie(movie: MovieEntity)
+
+    @Query("DELETE FROM movies WHERE created_at <= (strftime('%s','now', '-30 day') * 1000)")
+    suspend fun deleteMovies()
 }
--- app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/MovieDetailDao.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/local/dao/MovieDetailDao.kt
@@ -8,13 +8,13 @@ import com.heyproject.sipalingnonton.data.local.entity.MovieDetailEntity
 
 @Dao
 interface MovieDetailDao {
-    @Query("SELECT A.*, B.is_favorite FROM movies_detail A INNER JOIN discover_movies B ON A.id = B.id WHERE A.id = :movieId")
+    @Query("SELECT A.*, B.is_favorite FROM movies_detail A INNER JOIN movies B ON A.id = B.id WHERE A.id = :movieId")
     suspend fun getMovieDetail(movieId: Int): MovieDetailEntity
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovieDetail(movieDetailEntity: MovieDetailEntity)
 
     @Query("DELETE FROM movies_detail WHERE created_at <= (strftime('%s','now', '-30 day') * 1000)")
-    suspend fun deleteMovieDetail()
+    suspend fun deleteMovieDetails()
 }
 
