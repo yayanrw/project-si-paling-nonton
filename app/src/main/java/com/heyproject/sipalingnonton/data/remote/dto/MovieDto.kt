544123c3b2754a5d0f48a79e7dc5a4a2af84ff34
yayanrw <yayanraw@gmail.com>
2022-08-21 17:48:02 +0700

    show only

--- app/src/main/java/com/heyproject/sipalingnonton/data/remote/dto/MovieDto.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/dto/MovieDto.kt
@@ -36,18 +36,14 @@ data class MovieDto(
 ) {
     fun toMovie(): Movie {
         return Movie(
-            adult = adult,
             backdropPath = backdropPath,
             genreIds = genreIds,
             id = id,
-            originalLanguage = originalLanguage,
             originalTitle = originalTitle,
             overview = overview,
-            popularity = popularity,
             posterPath = posterPath,
             releaseDate = releaseDate,
             title = title,
-            video = video,
             voteAverage = voteAverage,
             voteCount = voteCount
         )
