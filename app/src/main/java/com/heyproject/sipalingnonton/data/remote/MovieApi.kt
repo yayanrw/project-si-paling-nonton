363a845f15775869853d5ef3b8dd8bee7a641395
yayanrw <yayanraw@gmail.com>
2022-09-04 09:09:43 +0700

    data remote housekeeping

--- app/src/main/java/com/heyproject/sipalingnonton/data/remote/MovieApi.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/MovieApi.kt
@@ -4,15 +4,15 @@ import com.heyproject.sipalingnonton.core.BEARER_TOKEN
 import com.heyproject.sipalingnonton.data.remote.response.MoviesResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MovieApi {
     @Headers("Authorization: Bearer $BEARER_TOKEN")
-    @GET("discover/movie?page={page}&sort_by={sortBy}&with_original_language={withOriginalLanguage}&with_genres={withGenre}")
+    @GET("discover/movie")
     suspend fun getMovies(
-        @Path("page") page: Int?,
-        @Path("sortBy") sortBy: String?,
-        @Path("withOriginalLanguage") withOriginalLanguage: String?,
-        @Path("withGenres") withGenres: String?
+        @Query("page") page: Int?,
+        @Query("sortBy") sortBy: String?,
+        @Query("withOriginalLanguage") withOriginalLanguage: String?,
+        @Query("withGenres") withGenres: String?
     ): MoviesResponse
 }
--- app/src/main/java/com/heyproject/sipalingnonton/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/RemoteDataSource.kt
@@ -1,9 +1,25 @@
 package com.heyproject.sipalingnonton.data.remote
 
 import com.heyproject.sipalingnonton.data.remote.dto.MovieDto
-
-private const val TAG = "RemoteDataSource"
+import com.heyproject.sipalingnonton.data.util.ApiResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class RemoteDataSource(private val movieApi: MovieApi) {
-    suspend fun getMovies(): List<MovieDto> = movieApi.getMovies(null, null, null, null).results
+    suspend fun getMovies(): Flow<ApiResponse<List<MovieDto>>> {
+        return flow {
+            try {
+                val response = movieApi.getMovies(1, null, null, null)
+                if (response.results.isNotEmpty()) {
+                    emit(ApiResponse.Success(response.results))
+                } else {
+                    emit(ApiResponse.Empty)
+                }
+            } catch (e: Exception) {
+                emit(ApiResponse.Error(e.toString()))
+            }
+        }.flowOn(Dispatchers.IO)
+    }
 }
