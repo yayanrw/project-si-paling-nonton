d8a4c6b96f60fd2630b6d1b7f0165f272bb6bd51
yayanrw <yayanraw@gmail.com>
2022-09-04 09:13:51 +0700

    clear domain

--- app/src/main/java/com/heyproject/sipalingnonton/domain/model/Genre.kt
+++
@@ -1,6 +0,0 @@
-package com.heyproject.sipalingnonton.domain.model
-
-data class Genre(
-    val id: Int,
-    val name: String
-)
--- app/src/main/java/com/heyproject/sipalingnonton/domain/model/MovieCredit.kt
+++
@@ -1,6 +0,0 @@
-package com.heyproject.sipalingnonton.domain.model
-
-data class MovieCredit(
-    val cast: List<Cast>,
-    val id: Int
-)
