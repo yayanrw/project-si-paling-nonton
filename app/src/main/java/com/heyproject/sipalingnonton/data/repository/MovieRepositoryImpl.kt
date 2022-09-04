3090458f56067ec601c118c637cb77d266cead24
yayanrw <yayanraw@gmail.com>
2022-09-04 09:30:58 +0700

    fix movierepo

--- app/src/main/java/com/heyproject/sipalingnonton/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/repository/MovieRepositoryImpl.kt
@@ -3,48 +3,36 @@ package com.heyproject.sipalingnonton.data.repository
 import com.heyproject.sipalingnonton.core.Resource
 import com.heyproject.sipalingnonton.data.local.LocalDataSource
 import com.heyproject.sipalingnonton.data.remote.RemoteDataSource
-import com.heyproject.sipalingnonton.domain.model.*
+import com.heyproject.sipalingnonton.data.remote.dto.MovieDto
+import com.heyproject.sipalingnonton.data.utils.ApiResponse
+import com.heyproject.sipalingnonton.data.utils.AppExecutors
+import com.heyproject.sipalingnonton.data.utils.NetworkBoundResource
+import com.heyproject.sipalingnonton.domain.model.Movie
 import com.heyproject.sipalingnonton.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import retrofit2.HttpException
-import java.io.IOException
 
 class MovieRepositoryImpl(
     private val localDataSource: LocalDataSource,
     private val remoteDataSource: RemoteDataSource,
+    private val appExecutors: AppExecutors
 ) : MovieRepository {
-    override fun getMovies(): Flow<Resource<List<Movie>>> = flow {
-        emit(Resource.Loading())
-        val localData = localDataSource.getMovies().map { it.toMovie() }
-        emit(Resource.Loading(data = localData))
-
-        try {
-            val remoteData = remoteDataSource.getMovies()
-            if (remoteData.isNotEmpty()) {
-                localDataSource.deleteMovies()
-                localDataSource.insertMovies(remoteData.map { it.toMovieEntity() })
+    override fun getMovies(): Flow<Resource<List<Movie>>> =
+        object : NetworkBoundResource<List<Movie>, List<MovieDto>>() {
+            override fun loadFromDB(): Flow<List<Movie>> {
+                return localDataSource.getMovies().map { list ->
+                    list.map { it.toMovie() }
+                }
             }
-        } catch (e: HttpException) {
-            emit(Resource.Error(
-                message = "Oops, something went wrong!",
-                data = localData
-            ))
-        } catch (e: IOException) {
-            emit(Resource.Error(
-                message = "Couldn't reach server, please check your internet connection.",
-                data = localData
-            ))
-        }
 
-        val newLocalData = localDataSource.getMovies().map { it.toMovie() }
-        emit(Resource.Success(newLocalData))
+            override fun shouldFetch(data: List<Movie>?): Boolean = true
 
-    }
+            override suspend fun createCall(): Flow<ApiResponse<List<MovieDto>>> =
+                remoteDataSource.getMovies()
 
-    override fun searchMovies(search: String): Flow<Resource<List<Movie>>> {
-        TODO("Not yet implemented")
-    }
+            override suspend fun saveCallResult(data: List<MovieDto>) {
+                localDataSource.insertMovies(data.map { it.toMovieEntity() })
+            }
 
+        }.asFlow()
 }
