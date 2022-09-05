0831d21927a40d1004b1f12071bb3a720e115192
yayanrw <yayanraw@gmail.com>
2022-09-05 15:20:30 +0700

    add moviedetail endpoint

--- app/src/main/java/com/heyproject/sipalingnonton/data/remote/MovieApi.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/MovieApi.kt
@@ -1,9 +1,11 @@
 package com.heyproject.sipalingnonton.data.remote
 
 import com.heyproject.sipalingnonton.core.BEARER_TOKEN
+import com.heyproject.sipalingnonton.data.remote.dto.MovieDetailDto
 import com.heyproject.sipalingnonton.data.remote.response.MoviesResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
@@ -15,4 +17,10 @@ interface MovieApi {
         @Query("withOriginalLanguage") withOriginalLanguage: String?,
         @Query("withGenres") withGenres: String?
     ): MoviesResponse
+
+    @Headers("Authorization: Bearer $BEARER_TOKEN")
+    @GET("movie/{movieId}")
+    suspend fun getMovieDetail(
+        @Path("movieId") movieId: Int
+    ): MovieDetailDto
 }
