b7f2d5f0f5b946307470e831eb92bafa3230e745
yayanrw <yayanraw@gmail.com>
2022-09-05 21:49:41 +0700

    add vm di

--- app/src/main/java/com/heyproject/sipalingnonton/di/AppModule.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.heyproject.sipalingnonton.di
 
 import com.heyproject.sipalingnonton.domain.usecase.MovieInteractor
 import com.heyproject.sipalingnonton.domain.usecase.MovieUsecase
+import com.heyproject.sipalingnonton.presentation.detail.DetailViewModel
 import com.heyproject.sipalingnonton.presentation.home.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -12,4 +13,5 @@ val usecaseModule = module {
 
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
+    viewModel { DetailViewModel(get()) }
 }
