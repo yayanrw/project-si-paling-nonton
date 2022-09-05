3f93e068425cf6a0a2173f646925c003fc13c976
yayanrw <yayanraw@gmail.com>
2022-09-05 13:58:40 +0700

    add dto

---
+++ app/src/main/java/com/heyproject/sipalingnonton/data/remote/dto/GenreDto.kt
@@ -0,0 +1,10 @@
+package com.heyproject.sipalingnonton.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class GenreDto(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
