6e93e2cb875d26a81caefe51b9bf29524721ce3d
yayanrw <yayanraw@gmail.com>
2022-09-08 10:14:03 +0700

    db encrypt

--- core/src/main/java/com/heyproject/core/core/MyConstant.kt
+++ core/src/main/java/com/heyproject/core/core/MyConstant.kt
@@ -4,3 +4,4 @@ const val BASE_URL = "https://api.themoviedb.org/3/"
 const val IMAGE_URL_ORIGIN = "https://image.tmdb.org/t/p/original"
 const val IMAGE_URL_SMALL = "https://image.tmdb.org/t/p/w500"
 const val TIMEOUT_CONNECTION: Long = 120
+const val DB_NAME = "MovieDB.db"
