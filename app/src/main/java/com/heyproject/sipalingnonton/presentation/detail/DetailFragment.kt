011adb2018d765661b5d32ef162c0f7945932b58
yayanrw <yayanraw@gmail.com>
2022-09-09 12:33:37 +0700

    set title

--- app/src/main/java/com/heyproject/sipalingnonton/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/heyproject/sipalingnonton/presentation/detail/DetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -33,6 +34,7 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         movie = args.movie
+        (activity as AppCompatActivity?)?.supportActionBar?.title = movie.title
 
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
