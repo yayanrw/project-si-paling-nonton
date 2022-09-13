7db23078a703747391d5be860622f28c3995d008
yayanrw <yayanraw@gmail.com>
2022-09-13 15:56:56 +0700

    remove

--- core/src/main/java/com/heyproject/core/data/utils/AppExecutors.kt
+++ core/src/main/java/com/heyproject/core/data/utils/AppExecutors.kt
@@ -8,13 +8,9 @@ import java.util.concurrent.Executors
 class AppExecutors @VisibleForTesting constructor(
     private val diskIO: Executor
 ) {
-
-    companion object;
-
     constructor() : this(
         Executors.newSingleThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
-
 }
